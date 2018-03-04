db541d1c1e51289c54adbbcf80c1931cb89230fc
ais047 <hursungwoo@yahoo.com>
2018-03-03 23:31:16 -0800

    stuff

---
+++ histogram_with_mask.py
@@ -0,0 +1,14 @@
+from matplotlib import pyplot as plt 
+import numpy as np 
+import argparse
+import cv2
+
+def plot_histogram(image, title, mask = None):
+    chans = cv2.split(image)
+    colors = ("b", "g", "r")
+    plt.figure()
+    plt.title(title)
+    plt.xlabel("Bins")
+    plt.ylabel("# of Pixels")
+
+    for (chan, color) in zip(chans)
